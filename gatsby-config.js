a2a67593527d290599393402f1292a12bc1ad363
Sreng Shuan <srengtrecom@yahoo.com>
2019-12-19 11:28:03 +0700

    initial commit on gatsby.

---
+++ gatsby-config.js
@@ -0,0 +1,48 @@
+module.exports = {
+  siteMetadata: {
+    title: `Innovative Eye Care`,
+    description: ``,
+    author: `@sreng-s`,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: "pages",
+        path: `${__dirname}/src/pages`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/static/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    `gatsby-plugin-catch-links`,
+    {
+      resolve: "gatsby-transformer-remark",
+      options: {
+        plugins: [], // just in case those previously mentioned remark plugins sound cool :)
+      },
+    },
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
+  ],
+}
--- migration-scripts/move.py
+++
@@ -1,7 +0,0 @@
-import os
-import shutil
-
-for dir in os.listdir('content/what-we-do'):
-    if dir!="_index.md":
-        print(dir)
-        shutil.copy("content/what-we-do/%s/_index.md" %dir, "content/what-we-do/%s.md" %dir)
