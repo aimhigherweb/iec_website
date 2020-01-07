f6e22ba889938c4642e7364c05b28e895f810d3f
Sreng Shuan <srengtrecom@yahoo.com>
2020-01-07 10:15:42 +0700

    some of changes.

--- gatsby-config.js
+++ gatsby-config.js
@@ -38,7 +38,23 @@ module.exports = {
     {
       resolve: "gatsby-transformer-remark",
       options: {
-        plugins: [], // just in case those previously mentioned remark plugins sound cool :)
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 590,
+            },
+          },
+          {
+            resolve: `gatsby-remark-responsive-iframe`,
+            options: {
+              wrapperStyle: `margin-bottom: 1.0725rem`,
+            },
+          },
+          `gatsby-remark-prismjs`,
+          `gatsby-remark-copy-linked-files`,
+          `gatsby-remark-smartypants`,
+        ],
       },
     },
     `gatsby-plugin-netlify-cms`,
