b04a441d85aa75485c584663ac84f71dff482010
Sreng Shuan <srengtrecom@yahoo.com>
2020-02-05 16:10:38 +0800

    update search page.

--- src/pages/search/index.js
+++ src/pages/search/index.js
@@ -3,18 +3,41 @@ import React, {Component} from 'react';
 import Header from '../../layouts/partials/header';
 import TopNav from "../../layouts/partials/topnav";
 import Footer from "../../layouts/partials/footer";
-import {Helmet} from "react-helmet/es/Helmet";
+import {graphql} from "gatsby";
 
 class Search extends Component {
+  state = {
+    hasResult: false,
+    isEnter: false,
+    searchKey: '',
+  };
+
+  componentDidMount() {
+    if (this.props.location.state) {
+      this.setState({searchKey: this.props.location.state.searchKey});
+    }
+  }
+
+  handleChange = (event) => {
+    this.setState({searchKey: event.target.value});
+  };
+
+  searchQuery = (e) => {
+    e.preventDefault();
+    if (e.key === 'Enter') {
+      this.setState({
+        isEnter: true,
+      });
+      console.log('do validate');
+    }
+  };
 
   render() {
+    // console.log(this.props.location.state.searchKey);
+    // const { data } = this.props;
+    // const list = data.allMarkdownRemark.edges;
     return (
       <>
-        <Helmet>
-          <script src="https://cdnjs.cloudflare.com/ajax/libs/fuse.js/3.2.0/fuse.min.js"></script>
-          <script src="https://cdnjs.cloudflare.com/ajax/libs/mark.js/8.11.1/jquery.mark.min.js"></script>
-          <script async type="text/javascript" src="/js/search.js" />
-        </Helmet>
         <Header />
         <TopNav />
 
@@ -27,10 +50,13 @@ class Search extends Component {
               <div className="intro-section__row">
                 <div className="intro-section__col intro-section__col_content">
                   <p>Search for patient resources, blog posts, and information about our practice</p>
-                  <form action="/search">
-                    <input id="search-query" name="s"/>
-                    <input type="submit" className="button" value="Search" />
-                  </form>
+                  <input
+                    id="search-query"
+                    value={this.state.searchKey}
+                    onChange={this.handleChange}
+                    onKeyPress={this.searchQuery}
+                  />
+                  <input type="submit" className="button" onClick={this.searchQuery} />
                 </div>
               </div>
             </div>
@@ -41,6 +67,14 @@ class Search extends Component {
             <div className="content-wrap">
               <div id="search-results">
                 <h3>Results</h3>
+                {
+                  (this.state.isEnter && !this.state.hasResult) &&
+                  <p>No matches found</p>
+                }
+                {
+                  (this.state.isEnter && this.state.searchKey === '') &&
+                  <p>Please enter a word or phrase above</p>
+                }
               </div>
             </div>
           </div>
@@ -53,3 +87,19 @@ class Search extends Component {
 }
 
 export default Search;
+
+export const searchQuery = graphql`
+    query SearchQuery {
+        allMarkdownRemark {
+            edges {
+                node {
+                    id
+                    frontmatter {
+                        title
+                    }
+                    excerpt(truncate: true, pruneLength: 300)
+                }
+            }
+        }
+    }
+`;
