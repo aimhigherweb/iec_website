10e1289f958632c813c2814bdf7e91482bad4abb
Sreng Shuan <srengtrecom@yahoo.com>
2020-02-06 19:37:41 +0800

    fix search func.

--- src/pages/search/index.js
+++ src/pages/search/index.js
@@ -1,14 +1,18 @@
 import React, {Component} from 'react';
+import {graphql, Link} from "gatsby";
+import {Helmet} from 'react-helmet'
+import Fuse from 'fuse.js';
 
 import Header from '../../layouts/partials/header';
 import TopNav from "../../layouts/partials/topnav";
 import Footer from "../../layouts/partials/footer";
-import {graphql} from "gatsby";
 
 class Search extends Component {
   state = {
-    hasResult: false,
-    isEnter: false,
+    hasResult: true,
+    isClicked: false,
+    list: [],
+    results: [],
     searchKey: '',
   };
 
@@ -16,47 +20,76 @@ class Search extends Component {
     if (this.props.location.state) {
       this.setState({searchKey: this.props.location.state.searchKey});
     }
+    setTimeout(() => {
+      const list = this.props.data.allMarkdownRemark.nodes;
+      if (list) {
+        this.setState({list});
+      }
+    }, 1000);
   }
 
-  handleChange = (event) => {
-    this.setState({searchKey: event.target.value});
-  };
-
-  searchQuery = (e) => {
-    e.preventDefault();
+  handleChange = async (e) => {
+    this.setState({
+      isClicked: false,
+      searchKey: e.target.value,
+    });
     if (e.key === 'Enter') {
-      this.setState({
-        isEnter: true,
-      });
-      console.log('do validate');
+      await this.handleSearch();
     }
   };
 
+  handleSubmit = async () => {
+    await this.handleSearch();
+  };
+
+  /**
+   * Search json data by using fuse.js
+   */
+  async handleSearch() {
+    const options = {
+      keys: ['frontmatter.title', 'frontmatter.category', 'internal.content'],
+    };
+    const fuse = new Fuse(this.state.list, options);
+    const results = await fuse.search(this.state.searchKey);
+
+    this.setState({
+      hasResult: results.length > 0,
+      isClicked: true,
+      results,
+    });
+  };
+
   render() {
-    // console.log(this.props.location.state.searchKey);
-    // const { data } = this.props;
-    // const list = data.allMarkdownRemark.edges;
     return (
       <>
-        <Header />
-        <TopNav />
+        <Helmet>
+          <title>Search Results</title>
+        </Helmet>
+        <Header/>
+        <TopNav/>
 
         <div className="intro-section" style={{backgroundImage: 'url(/images/intro-bg.png)'}}>
           <div className="container">
             <div className="intro-section__wrap">
               <div className="intro-section__heading">
-                <h2 className="intro-section__title">Search </h2>
+                <h2 className="intro-section__title">Search</h2>
               </div>
               <div className="intro-section__row">
                 <div className="intro-section__col intro-section__col_content">
                   <p>Search for patient resources, blog posts, and information about our practice</p>
                   <input
                     id="search-query"
+                    name="s"
                     value={this.state.searchKey}
                     onChange={this.handleChange}
-                    onKeyPress={this.searchQuery}
+                    onKeyPress={(e) => this.handleChange(e)}
                   />
-                  <input type="submit" className="button" onClick={this.searchQuery} />
+                  <input
+                    type="button"
+                    className="button"
+                    value="Search"
+                    onClick={this.handleSubmit}
+                  />
                 </div>
               </div>
             </div>
@@ -68,19 +101,39 @@ class Search extends Component {
               <div id="search-results">
                 <h3>Results</h3>
                 {
-                  (this.state.isEnter && !this.state.hasResult) &&
+                  (this.state.isClicked && !this.state.hasResult) &&
                   <p>No matches found</p>
                 }
                 {
-                  (this.state.isEnter && this.state.searchKey === '') &&
+                  (this.state.isClicked && this.state.searchKey === '') &&
                   <p>Please enter a word or phrase above</p>
+                }
+                {
+                  this.state.results && (
+                    <>
+                      {
+                        this.state.results.map((result, i) => (
+                          <div key={i}>
+                            <h4>
+                              <Link to={`/${result.parent.relativeDirectory}/${result.parent.name}`}>
+                                {result.frontmatter.title}
+                              </Link>
+                            </h4>
+                            <p>
+                              {result.excerpt}
+                            </p>
+                          </div>
+                        ))
+                      }
+                    </>
+                  )
                 }
               </div>
             </div>
           </div>
         </div>
 
-        <Footer />
+        <Footer/>
       </>
     );
   }
@@ -89,15 +142,22 @@ class Search extends Component {
 export default Search;
 
 export const searchQuery = graphql`
-    query SearchQuery {
+    {
         allMarkdownRemark {
-            edges {
-                node {
-                    id
-                    frontmatter {
-                        title
+            nodes {
+                frontmatter {
+                    title
+                    category
+                }
+                excerpt(truncate: true, pruneLength: 300)
+                internal {
+                    content
+                }
+                parent {
+                    ... on File {
+                        name
+                        relativeDirectory
                     }
-                    excerpt(truncate: true, pruneLength: 300)
                 }
             }
         }
