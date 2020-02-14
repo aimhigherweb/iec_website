9406314302340741ef213f48bd86e3972fca528f
Jake Brown <jake@eyespacelenses.com>
2020-02-14 16:10:57 +1030

    Added gatsby-plugin-nprogress. Closes #25.

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,9 @@ module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
     {
+      resolve: `gatsby-plugin-nprogress`,
+    },
+    {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: "pages",
