53a0367491d47dc0fd690d5ab0ad58859db563f8
dave <dave@daves-MacBook-Pro.local>
2020-09-18 11:02:43 +0930

    gatsby-config (sourceNodes for author)

--- gatsby-config.js
+++ gatsby-config.js
@@ -83,4 +83,7 @@ module.exports = {
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
   ]
+  mapping: {
+    "MarkdownRemark.fields.author": `MarkdownRemark`,
+  },
 }
