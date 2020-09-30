e7bb6582d01b2de16c3cf501468f0ad83e9e391d
dave <dave@daves-MacBook-Pro.local>
2020-09-30 11:40:04 +0930

    layouts/index, state/SessionWrapper (global layout)

---
+++ src/state/SessionWrapper.js
@@ -0,0 +1,25 @@
+import React, { createContext, useContext, useState } from "react"
+
+const AppContext = createContext()
+
+export const SessionWrapper = ({ children }) => {
+  const [current, setCurrent] = useState({
+    index: 99,
+    articles: ["aaa", "bbb", "ccc"],
+  })
+
+  return (
+    <>
+      <AppContext.Provider
+        value={{
+          current: current,
+          setCurrent: setCurrent,
+        }}
+      >
+        {children}
+      </AppContext.Provider>
+    </>
+  )
+}
+
+export const useSession = () => useContext(AppContext)
