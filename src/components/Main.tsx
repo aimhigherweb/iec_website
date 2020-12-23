b65841a733440ad855ac431ba7c55a8c25cb33b6
dave <dave@daves-MacBook-Pro.local>
2020-12-23 11:33:05 +1030

    src/components/Main (show header filler on mobile)

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -319,7 +319,7 @@ const MainDiv = (
           <BookingIframe src={bookingUrl()}></BookingIframe>
         </MainAppointment>
       )}
-      {!showFull && <MainHeaderFiller />}
+      {(!showFull || match) && <MainHeaderFiller />}
       {showFull && (
         <MainFooter searchMode={showSearch}>
           <MainSearch>
